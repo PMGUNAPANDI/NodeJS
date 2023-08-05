1bc7135649ee143106ebfb33376ba2d6b42d1b94
GUNAPANDI PM <113769754+Harisankar7813@users.noreply.github.com>
2023-08-05 14:01:32 +0530

    Added New Files

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
-// npm - global command, comes with node
-//npm --version
+const EventEmitter = require('events');
 
+const customEmitter = new EventEmitter()
 
-//local depency - use it only in this particular project
-//npm i <packageName>
+customEmitter.on('response',(name,id) => {
+    console.log(`data received user:${name} with id: ${id}`);
+});
+customEmitter.on('response',() => {
+    console.log(`some other logic here`);
+});
 
-//global depency - use it in any project 
-//npm install -g <packageName>
-//sudo install -g <packageName> (mac)
-
-
-// package.json - manifest file (stores important info about project/package)
-// manual approach (create package.json in the root, create properties etc)
-//npm init (step by step, press enter to skip)
-// npm init -y (everything default)
-
-
-const _ = require('lodash')
-
-const items = [1,[2,[3,[4]]]];
-const newItems =_.flattenDeep(items);
-console.log(newItems);
-
+customEmitter.emit('response', 'Tarcin',1)
